a013013c1190a8d863550f6371e2dcd55c16b271
Dani1lopez <dalcarazlopez163@gmail.com>
2026-03-02 16:46:56 +0100

    fix: use gunicorn and collectstatic in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
+echo "Collecting static files..."
+python manage.py collectstatic --noinput
+
 echo "Aplicando migraciones..."
 python manage.py migrate --noinput
 
-echo "Arrancado servidor..."
-exec python manage.py runserver 0.0.0.0:8000
+echo "Arrancando servidor..."
+exec gunicorn accessledger.wsgi --bind 0.0.0.0:8080 --log-file -
