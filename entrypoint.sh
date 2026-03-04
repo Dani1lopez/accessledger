d3b01b850c91ab22021d14533533ef62314ad01a
Dani1lopez <dalcarazlopez163@gmail.com>
2026-03-04 09:53:46 +0100

    fix: use runserver in dev, gunicorn in production

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,8 @@ python manage.py collectstatic --noinput --clear
 echo "Aplicando migraciones..."
 python manage.py migrate --noinput
 echo "Arrancando servidor..."
-exec gunicorn accessledger.wsgi --bind 0.0.0.0:${PORT:-8080} --log-file -
+if [ "$DEBUG" = "True" ]; then
+    exec python manage.py runserver 0.0.0.0:8000
+else
+    exec gunicorn accessledger.wsgi --bind 0.0.0.0:${PORT:-8080} --log-file -
+fi
